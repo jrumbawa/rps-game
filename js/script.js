4f29ca0445640f7dbe1f37305df3864ed9179b37
Jomar Rumbawa <hello@jomarrumbawa.com>
2025-02-06 19:49:13 -0800

    Create playRound function

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let playerScore = 0;
+let humanScore = 0;
 let computerScore = 0;
 
 function getComputerChoice() {
@@ -8,6 +8,29 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-  const choice = prompt("Rock, paper or scissors?");
-  return choice.toLowerCase();
+  const choice = prompt("Rock, paper or scissors?").toLowerCase();
+  return choice;
 }
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+function playRound(humanChoice, computerChoice) {
+  if ((humanChoice === "rock") & (computerChoice === "scissors")) {
+    console.log("You win! Rock beats Scissors");
+  } else if ((humanChoice === "rock") & (computerChoice === "paper")) {
+    console.log("You lose! Paper beats Rock");
+  } else if ((humanChoice === "paper") & (computerChoice === "rock")) {
+    console.log("You win! Paper beats Rock");
+  } else if ((humanChoice === "paper") & (computerChoice === "scissors")) {
+    console.log("You lose! Scissors beats Paper");
+  } else if ((humanChoice === "scissors") & (computerChoice === "paper")) {
+    console.log("You win! Scissors beats paper");
+  } else if ((humanChoice === "scissors") & (computerChoice === "rock")) {
+    console.log("You lose! Rock beats Scissors");
+  } else {
+    console.log("It's a tie!");
+  }
+}
+
+playRound(humanSelection, computerSelection);
